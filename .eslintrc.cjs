82ec5f2104d41c9f3e6588f05af8cf20da5e508c
kelwinv <kelwin08@gmail.com>
2023-07-28 18:38:56 -0300

    styles(tailwind)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,12 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', "prettier"],
+  plugins: ['react-refresh', 'tailwindcss'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
